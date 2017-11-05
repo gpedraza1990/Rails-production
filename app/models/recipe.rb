b05044269ae5cae39a3caadc74e4c2032a91b34f
gabriel1990 <gabriel1990@tutanota.com>
2017-11-05 10:50:17 -0500

    remove all logic from controller recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,8 +3,7 @@ class Recipe
   include HTTParty
   default_params fields "smallIcon, shortDescription" q: "search"
   format  :json
-  key_value = ENV['FOOD2FORK_KEY']
-  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com:80'
+  
   base_uri "https://api.coursera.org/api/catalog.v1/courses"
     
   def self.for term
