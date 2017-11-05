5a6aa7a902ed7317d22a930bb4dd0ef547bb861d
gabriel1990 <gabriel1990@tutanota.com>
2017-11-05 10:29:49 -0500

    remove all logic from controller recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,6 @@
 class RecipesController < ApplicationController
   def index
     $search_term='chocolate'
-    if params[:search].present?
-      $search_term=params[:search]
-    end	 
-     @recipes = Recipe.for($search_term)	
+    	
   end
 end
