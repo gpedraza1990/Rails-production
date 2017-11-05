67c08f97fed1c86347efaee6f6260cc94e5c0cbd
gabriel1990 <gabriel1990@tutanota.com>
2017-11-05 10:54:45 -0500

    remove all logic from controller recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,5 @@
 class RecipesController < ApplicationController
   def index
-    $search_term='jhu'
-    
-     @recipes = Recipe.for($search_term)	
+    	
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,13 +1,6 @@
 class Recipe
 
   include HTTParty
-  default_params fields "smallIcon, shortDescription" q: "search"
-  format  :json
   
-  base_uri "https://api.coursera.org/api/catalog.v1/courses"
-    
-  def self.for term
-    get("+",query: { query: term})["elements"]
-  end
 
 end
