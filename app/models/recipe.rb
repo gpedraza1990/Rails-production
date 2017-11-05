803c38ff2e5015a044dcf8da7e93597862d7d6a8
gabriel1990 <gabriel1990@tutanota.com>
2017-11-05 14:38:36 -0500

    remove all logic from controller recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,12 +4,12 @@ class Recipe
   
   format  :json
   key_value = ENV['FOOD2FORK_KEY'] || 'db2f3b5a9b50dc1a3cf8fe47d6326c1c'
-  default_params  key: key_value, fields: "recipes,count"
+  default_params  key: key_value
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api/"
     
   def self.for (term)
-    get("/search", :query => { :q => term})["elements"]
+    get("/search", :query => { :q => term})
   end
 
 end
