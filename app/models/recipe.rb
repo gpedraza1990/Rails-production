17a5f29a47cde8aa4d833571c581125e9b4d80a0
gabriel1990 <gabriel1990@tutanota.com>
2017-11-05 14:13:39 -0500

    remove all logic from controller recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,7 +8,7 @@ class Recipe
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api/"
     
-  def self.for term
+  def self.for (term)
     get("/search",query: { q: term})
   end
 
