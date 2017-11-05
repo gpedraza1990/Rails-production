838b2fddf2d93789438f49b770e2d4fbde98bdf7
gabriel1990 <gabriel1990@tutanota.com>
2017-11-05 14:51:03 -0500

    remove all logic from controller recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,7 +9,7 @@ class Recipe
   base_uri "http://#{hostport}/api/"
     
   def self.for (term)
-    get("/search", :query => { :q => term})["elements"]
+    get("/search", :query => { :q => term})
   end
 
 end
